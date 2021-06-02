693fee0f47707febb089f68b31995bf379cac931
git202122 <84128742+git202122@users.noreply.github.com>
2021-06-02 19:15:54 +0530

    Updated app_java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        System.out.println( "Hello World !!! Learning Jenkins" );
+        System.out.println( "Hello World !!! Learning Jenkinssss" );
         assertTrue( true );
     }
 }
